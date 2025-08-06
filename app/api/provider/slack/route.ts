063915ff3a68f725f5722ecaaaf913d855db5d68
Aelithron <aelithron@gmail.com>
2025-08-06 04:48:40 -0600

    Attempt to add Slack callback things :3 unsigned because i'm on my mac and haven't gotten a pgp key on here

--- app/api/provider/slack/route.ts
+++ app/api/provider/slack/route.ts
@@ -1,9 +1,28 @@
 import { auth } from "@/auth";
 import { UserDoc } from "@/universalstatus";
 import client, { getUserDoc } from "@/utils/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
+
+export async function GET(req: NextRequest) {
+  const session = await auth();
+  if (!session || !session.user) return NextResponse.json({ error: "unauthorized", message: "Not logged in, please log in to continue." }, { status: 401 });
+  if (session.user.email === null || session.user.email === undefined) return NextResponse.json({ error: "invalid_profile", message: "You don't have an email in your profile, try logging back in." }, { status: 400 });
+  const userDoc = await getUserDoc(session.user.email);
+  if (!userDoc) return NextResponse.json({ error: "invalid_user", message: "The provided user doesn't exist, try logging back in." }, { status: 400 });
+  const slackTempCode = req.nextUrl.searchParams.get("code");
+  if (!slackTempCode) return NextResponse.json({ error: "missing_code", message: "URL is missing a code parameter, please try again with Slack auth!" }, { status: 400 });
+
+  const slackRes = await fetch("https://slack.com/api/oauth.v2.access", { method: "POST", body: JSON.stringify({ code: slackTempCode, client_id: process.env.AUTH_SLACK_ID, client_secret: process.env.AUTH_SLACK_SECRET }) });
+  if (!slackRes) return NextResponse.json({ error: "get_token_failed", message: "Failed to get the Slack authorization token, make sure the code is correct!" }, { status: 500 });
+  const slackBody = await slackRes.json();
+  if (!slackBody.access_token) return NextResponse.json({ error: "get_token_failed", message: "Failed to get the Slack authorization token, make sure the code is correct!" }, { status: 500 });
+  await client.db(process.env.MONGODB_DB).collection<UserDoc>("statuses").updateOne({ user: session.user.email }, {
+    $set: { slackToken: slackBody.access_token }
+  });
+  return NextResponse.redirect("/settings");
+}
 
 export async function DELETE() {
   const session = await auth();
