c1588f944284293fc2efa15e16bbf6d53949354d
Nova <aelithron@gmail.com>
2025-09-13 20:42:21 -0600

    Add an untested GitHub status mutation

--- utils/platforms.ts
+++ utils/platforms.ts
@@ -1,5 +1,9 @@
 import { Emoji } from "emoji-type";
 import { getUserDoc } from "./db";
+import { ApolloClient, gql } from "@apollo/client";
+import { InMemoryCache } from "@apollo/client";
+import { HttpLink } from "@apollo/client";
+import { ApolloLink } from "@apollo/client";
 
 export async function updateSlack(user: string, status: string, emoji: Emoji): Promise<{ message: string, error: boolean }> {
   const userDoc = await getUserDoc(user);
@@ -32,17 +36,31 @@ export async function updateSlack(user: string, status: string, emoji: Emoji): P
 export async function updateGithub(user: string, status: string, emoji: Emoji): Promise<{ message: string, error: boolean }> {
   const userDoc = await getUserDoc(user);
   if (!userDoc) return { message: "The provided user doesn't exist!", error: true };
-  try {
-    
-    /*
-      if (!error) {
-        return { message: "Status updated successfully!", error: false };
-      } else {
-        return { message: `Error from the GitHub API: ${error}`, error: true };
+  const ghClientAuth = new ApolloLink((operation, forward) => {
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        authorization: userDoc.githubPAT,
+      },
+    }));
+    return forward(operation);
+  });
+  const gqlClient = new ApolloClient({
+    link: ApolloLink.from([ghClientAuth, new HttpLink({ uri: "https://api.github.com/graphql" })]),
+    cache: new InMemoryCache()
+  });
+  const statusRes = await gqlClient.mutate({
+    mutation: gql`
+      mutation {
+        changeUserStatus(input: { clientMutationId: "universal-status-${process.hrtime.bigint()}", emoji: "${emoji}", message: "${status}" }) {
+          clientMutationId
+        }
       }
-    */
-  } catch (e) {
-    console.warn(`Unknown error in pushing status to GitHub for user "${user}"!\n${e}`);
-    return { message: `Unknown error, contact the site administrator and tell them to check the console!`, error: true };
+    `,
+  });
+  if (!statusRes.error) {
+    return { message: "Status updated successfully!", error: false };
+  } else {
+    return { message: `Error from the GitHub API: ${statusRes.error}`, error: true };
   }
 }
